2f64a76ae68fb78dd47299d6197c4943600d030f
Namit <namit.sg@perfios.com>
2022-08-17 14:24:31 +0530

    hello

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCalculator {
+	Calculator obj;
+	
+	@Test
+	public void testAdd() {
+		obj = new Calculator();
+		assertEquals(30, obj.add(10,20));
+	}
+}
